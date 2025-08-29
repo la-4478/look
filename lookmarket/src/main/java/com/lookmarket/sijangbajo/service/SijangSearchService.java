7c908908af07c1d352534ae4e26513db374176c7
la-4478 <leg58401@naver.com>
2025-08-29 15:46:08 +0900

    ㅁㄴㅇㅁㄴㅇ

--- lookmarket/src/main/java/com/lookmarket/sijangbajo/service/SijangSearchService.java
+++ lookmarket/src/main/java/com/lookmarket/sijangbajo/service/SijangSearchService.java
@@ -187,27 +187,7 @@ public class SijangSearchService {
 
 	    return courseList;
 	}
-	public List<Map<String, Object>> fetchFestivalListByRegionName(String regionName) {
-	    String areaCode;
-	    	
-	    if (regionName.matches("\\d+")) {
-	        areaCode = regionName;
-	    } else {
-	        String normalized = normalizeRegionName(regionName);
-	        areaCode = areaCodeMap.get(normalized);
-	    }
-	    System.out.println("areaCode :" + areaCode );
-
-	    if (areaCode == null) {
-	        throw new IllegalArgumentException("유효하지 않은 지역명입니다: " + regionName);
-	    }
-
-	    return fetchFestivalList(areaCode);
-	}
-
-
-
-
+	
 	public double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
 	    double earthRadius = 6371.0; // km 단위
 	    double dLat = Math.toRadians(lat2 - lat1);
@@ -329,52 +309,6 @@ public class SijangSearchService {
 
 	    return festivalList;
 	}
-	private String normalizeRegionName(String input) {
-        if (input == null) return null;
-
-        if (input.contains("서울")) return "서울특별시";
-        if (input.contains("부산")) return "부산광역시";
-        if (input.contains("인천")) return "인천광역시";
-        if (input.contains("대전")) return "대전광역시";
-        if (input.contains("대구")) return "대구광역시";
-        if (input.contains("광주")) return "광주광역시";
-        if (input.contains("울산")) return "울산광역시";
-        if (input.contains("세종")) return "세종특별자치시";
-        if (input.contains("경기")) return "경기도";
-        if (input.contains("강원")) return "강원도";
-        if (input.contains("충북")) return "충청북도";
-        if (input.contains("충남")) return "충청남도";
-        if (input.contains("경북")) return "경상북도";
-        if (input.contains("경남")) return "경상남도";
-        if (input.contains("전북")) return "전라북도";
-        if (input.contains("전남")) return "전라남도";
-        if (input.contains("제주")) return "제주특별자치도";
-
-        return input;
-    }
-	// 지역명 → areaCode 매핑
-	private static final Map<String, String> areaCodeMap = new HashMap<>();
-
-	static {
-	    areaCodeMap.put("1", "서울");
-	    areaCodeMap.put("인천", "2");
-	    areaCodeMap.put("대전", "3");
-	    areaCodeMap.put("대구", "4");
-	    areaCodeMap.put("광주", "5");
-	    areaCodeMap.put("부산", "6");
-	    areaCodeMap.put("울산", "7");
-	    areaCodeMap.put("세종특별자치시", "8");
-	    areaCodeMap.put("경기도", "31");
-	    areaCodeMap.put("강원도", "32");
-	    areaCodeMap.put("충청북도", "33");
-	    areaCodeMap.put("충청남도", "34");
-	    areaCodeMap.put("경상북도", "35");
-	    areaCodeMap.put("경상남도", "36");
-	    areaCodeMap.put("전라북도", "37");
-	    areaCodeMap.put("전라남도", "38");
-	    areaCodeMap.put("제주특별자치도", "39");
-	}
-
 	public List<Map<String, Object>> fetchAllFestivals() {
 		// TODO Auto-generated method stub
 		return null;
